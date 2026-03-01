bd5fbe275d9e4d0ca93ed35ec28ecf66da89f161
Kristian <sememikrobilja@gmail.com>
2026-03-01 17:10:30 +0100

    Users API: remove admin-only restriction for internal org management

--- modules/users/users.routes.ts
+++ modules/users/users.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { auth } from "../../middleware/auth";
 import { orgScope } from "../../middleware/orgScope";
-import { requireRole } from "../../middleware/requireRole";
 import { validate } from "../../utils/validate";
 import { createUserSchema, updateUserSchema } from "./users.schemas";
 import { getUsers, patchUser, postUser } from "./users.controller";
 
 const router = Router();
 
-router.use(auth(), orgScope, requireRole(["ADMIN"]));
+router.use(auth(), orgScope);
 
 router.get("/", getUsers);
 router.post("/", validate(createUserSchema), postUser);
